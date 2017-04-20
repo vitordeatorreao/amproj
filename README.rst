51b51b0e49253f143bef455e55ea05e358939c90
Vítor de Albuquerque Torreão <vitordeatorreao@gmail.com>
2017-04-19 21:54:01 -0300

    First commit \o/

---
+++ README.rst
@@ -0,0 +1,5 @@
+AM Proj
+--------
+
+This is the project for the Machine Learning class in the UFPE's MSc in
+Computer Science, in the first semester of 2017.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+description = 'The 2017 UFPE\'s machine learning class project'
+authors = ['Vitor Torreao <vat@cin.ufpe.br>',
+           'Avyner Lucena <ahbfl@cin.ufpe.br>',
+           'Jorge Linhares <jhcl@cin.ufpe.br>']
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except(IOError):
+    	return description
+
+setup(name='amproj',
+      version='0.1',
+      description=description,
+      long_description=readme(),
+      url='http://github.com/vitordeatorreao/amproj',
+      author=', '.join(authors),
+      author_email='',
+      license='GPL-2.0',
+      packages=['amproj'],
+      zip_safe=False)
